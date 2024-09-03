2f19aa7db2819bb49b795f53dc8683a84363dec3
AkshayDaharwal <akshay30.ad@gmail.com>
2024-09-03 17:02:29 +0530

    BackendPortal_Api

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(errorHandler);
 app.use(fileupload());
